5218e0f7f30ce3bff899a9c935c288610d16c960
poojangit <poojang147@gmail.com>
2025-01-17 16:34:36 +0530

    [POOJA] ArrayList: method calling

--- ArrayList/ArrayListMain.java
+++ ArrayList/ArrayListMain.java
@@ -6,8 +6,13 @@ public class ArrayListMain {
         arr.add(1);
         arr.add(2);
         arr.add(3);
-
+        arr.add(4);
+        arr.add(5);
+        System.out.println(arr.getValue(0));
         arr.display();
+        arr.remove(0);
+        arr.display();
+       System.out.println( arr.size());
 
     }
 }
